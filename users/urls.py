f8c797ab6c24edf9e9835b47196a931f71a0296e
Edwin Mugambi <edwinnjeru77@gmail.com>
2023-01-18 16:49:03 +0300

    Created the signup page :artist:

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 #users/urls.py
 from django.urls import path
-from .views import home
+from .views import home,SignUp
 
 urlpatterns = [
-    path('', home, name = "home")
+    path('', home, name = "home"),
+    path("signup/", SignUp.as_view(), name="signup")
 ]
